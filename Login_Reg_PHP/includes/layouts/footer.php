17ff4529a9f4888b1c4ca29c024e5a527dde4cd2
Komolafe Olorundara <49000733+Flystunna@users.noreply.github.com>
2019-06-11 11:13:04 +0100

    Add files via upload

---
+++ Login_Reg_PHP/includes/layouts/footer.php
@@ -0,0 +1,7 @@
+<!-- JQuery -->
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+  </div> <!-- closing container div -->
+</body>
+</html>
